1681d7dbde1504dc66210f9c87f3aa7ba5923680
Vedant Bande <vedantbande2002@gmail.com>
2024-07-26 00:20:27 +0530

    solved security issues

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const http = require("http");
-// const helmet=require("helmet")
 const socketio = require("socket.io");
 const messageBox = require("./utils/messages");
 const {
@@ -15,7 +14,17 @@ const server = http.createServer(app);
 
 require('dotenv').config();
 // Use Helmet to set various security headers
-// app.use(helmet());
+app.use((req, res, next) => {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+  res.setHeader("Referrer-Policy", "no-referrer");
+  res.setHeader("Permissions-Policy", "geolocation=(self), microphone=()"); // Customize permissions as needed
+  res.setHeader("Cache-Control", "no-store"); // Control caching behavior
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 const io = socketio(server);
